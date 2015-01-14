d837ec9b507854437477bafbff496e851d647b1c
Vergil102 <vergil.300@yandex.ru>
2015-01-14 19:54:41 +0500

    loolo

---
+++ 23/1234.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ne {
+	
+	public static void main(String[] arg){
+	
+			System.out.println("Введите что будете переводить, километры 1, мили 2");
+			
+			int q = q.nextInt
+		
+			Scanner q = new Scanner(System.in);
+			
+			if(q, q == 1){
+				
+				System.out.println("Введите километры");
+				
+				Scanner w = new Scanner(System.in);
+				
+				miles = Math.sqrt(w * 0.62);
+				
+				System.out.println("Мили" +miles);
+			}
+				
+
+				
+	}			
+}
